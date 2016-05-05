65461d3f3aa02e043645596bc91b02d58288c458
Joe Greubel <jgreub@gmail.com>
2016-05-04 23:03:11 -0700

    Add basic javascript unit test capabilities
    - Need to mock controller dependencies

---
+++ karma.conf.js
@@ -0,0 +1,25 @@
+module.exports = function(config) {
+  config.set({
+
+    files: [
+      'node_modules/angular/angular.min.js',
+      'node_modules/angular-mocks/angular-mocks.js',
+      'src/main/js/**/*.js',
+      'src/test/js/**/*.js'
+    ],
+
+    singleRun: true,
+
+    frameworks: ['jasmine'],
+
+    browsers: ['PhantomJS'],
+
+    plugins: [
+      'karma-chrome-launcher',
+      'karma-jasmine',
+      'karma-phantomjs-launcher'
+    ],
+
+    reporters: ['dots']
+  });
+};
