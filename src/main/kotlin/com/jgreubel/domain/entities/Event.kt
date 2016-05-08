352bcd9c64236cf176cc7a968d137240fa406823
Joe Greubel <jgreub@gmail.com>
2016-05-07 20:28:21 -0700

    Fix sequence generation starting at 1 instead of 50
    Closes #21

--- src/main/kotlin/com/jgreubel/domain/entities/Event.kt
+++ src/main/kotlin/com/jgreubel/domain/entities/Event.kt
@@ -1,12 +1,14 @@
 package com.jgreubel.domain.entities
 
-import javax.persistence.*
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
 
 @Entity
 data class Event(
     @Id
-    @SequenceGenerator(name = "event_id_seq", sequenceName = "event_id_seq")
-    @GeneratedValue(generator = "event_id_seq", strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0,
     var description: String = ""
 )
